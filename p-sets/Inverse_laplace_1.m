719fa9914f3a759648941352e0e69ce5d56e4590
Ritwik Raha <44690292+ritwikraha@users.noreply.github.com>
2018-11-25 00:54:37 +0530

    Update Inverse_laplace_1.m

--- p-sets/Inverse_laplace_1.m
+++ p-sets/Inverse_laplace_1.m
@@ -16,4 +16,5 @@ d4=[1 2];
 G4=tf(n4,d4);
 G5=feedback(G1,G2,-1);
 G6=feedback(G5,G3,-1);
-G7=series(G6,G4)
+G7=series(G6,G4);
+g=ilaplace(G7)%here the ilaplace function fails to recognize the tf object G7
