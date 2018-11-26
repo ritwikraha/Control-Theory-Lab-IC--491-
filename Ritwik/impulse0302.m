55fa529d94efc7c01c0ad56c3a000fcbff5415f4
Ritwik Raha <44690292+ritwikraha@users.noreply.github.com>
2018-11-26 21:47:37 +0530

    Add files via upload
    please note that peak time is displaying the value from the undershoot for impulse response

--- Ritwik/impulse0302.m
+++ Ritwik/impulse0302.m
@@ -6,3 +6,7 @@ d2=[1];
 H1=tf(n2,d2);
 G2=feedback(G1,H1,-1);
 impulse(G2)
+%info is used to show the peak time and the settling time 
+%right click on the figure to open characteristics
+%Voila!
+info (G2)
