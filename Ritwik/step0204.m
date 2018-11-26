15d76a50672f7b841c8e96dbb42930e46206a1ae
Ritwik Raha <44690292+ritwikraha@users.noreply.github.com>
2018-11-26 21:55:01 +0530

    individuality of stepinfo function could not be realized

--- Ritwik/step0204.m
+++ Ritwik/step0204.m
@@ -4,5 +4,8 @@ for k=[0.1 0.4 0.7 1 2];
     d1=[1 2*k 1];
     g3=tf(n1,d1);
     step (g3);
+    %how to find stepinfo for different values of k from the transfer
+    %function
+    stepinfo(g3)
     hold on
 end
