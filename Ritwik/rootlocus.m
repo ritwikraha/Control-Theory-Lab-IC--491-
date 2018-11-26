f30849c2e06eddcc8171f6ea26483adf22328108
Ritwik Raha <44690292+ritwikraha@users.noreply.github.com>
2018-11-26 21:51:38 +0530

    Add files via upload
    different values of Wn and zeta

--- Ritwik/rootlocus.m
+++ Ritwik/rootlocus.m
@@ -2,5 +2,11 @@ n=[1];
 d=[1 1.1 10.3 5 0];
 G=tf(n,d);
 rlocus(G)
+%damp function is used to find the natural frequency and zeta of a transfer
+%function 
+%damp(G)
+
+%this gives two coulmn vectors having different values of the same
+[Wn,zeta] = damp(G)
 [k,p]=rlocfind(G)
-sgrid(0.5,[])
+sgrid(1.0,[])
